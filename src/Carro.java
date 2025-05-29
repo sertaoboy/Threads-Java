fc71087d416556f3858e60bcca2bb61c9fd518cc
Raul H <raul.dev@hotmail.com>
2025-05-29 00:17:11 -0300

    Classes de exemplo

---
+++ src/Carro.java
@@ -0,0 +1,22 @@
+public class Carro implements Runnable{
+
+    private int id;
+    private Semaforo semaforo;
+
+    public Carro(int id, Semaforo semaforo) {
+        this.id=id;
+        this.semaforo = semaforo;
+    }
+
+    @Override
+    public void run(){
+        while(true) {
+            semaforo.atravessar(id);
+            try {
+                Thread.sleep(1000 + (int) (Math.random() * 4000));
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/Semaforo.java
@@ -0,0 +1,47 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Semaforo implements Runnable{
+
+    private boolean aberto = false;
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition c = lock.newCondition();
+
+    @Override
+    public void run() {
+        while(true) {
+            lock.lock();
+            try {
+                aberto = !aberto;
+                System.out.println("Semáforo aberto: " + aberto);
+            } finally {
+                lock.unlock();
+            }
+            try{
+                Thread.sleep(1000 + (int) (Math.random() * 4000));
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void atravessar(int id) {
+        lock.lock();
+        try {
+            while(!aberto) {
+                System.out.println("[Carro " + id + "] Aguardando");
+                if(aberto) {
+                    c.signalAll();
+                }
+                try {
+                    c.await();
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("[Carro " + id +"] Atravessou");
+        } finally {
+            lock.unlock();
+        }
+    }
+}
