b26748a767b6b10065f76a1b335fc6b09f885868
Raul H <raul.dev@hotmail.com>
2025-05-29 01:39:29 -0300

    observando o funcionamento de tarefas futuras com o exemplo de busca de arquivo

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -1,5 +1,7 @@
+import java.io.File;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
 
 public class Aplicacao {
 
@@ -44,6 +46,22 @@ public class Aplicacao {
             e3.execute(carro);
         }
         e3.shutdown();
+
+        ExecutorService executorSimples2 = Executors.newSingleThreadExecutor();
+        Localizador l = new Localizador("/home/raul/Códigos/ThreadsJava/ConcurrentsThreads1/src", "arquivoteste.txt");
+        FutureTask<File> task = new FutureTask<File>(l);
+        executorSimples2.execute(task);
+        while(!task.isDone()) {
+            System.out.println("Aguardando localização do arquivo...");
+            Thread.sleep(2000);
+        }
+        File file = task.get();
+        if(file!=null) {
+            System.out.println("Arquivo encontrado: " +file.getPath());
+        }else{
+            System.out.println("Arquivo não encontrado.");
+        }
+        executorSimples2.shutdown();
 
 
     }
