36d27a2f3fad8ab4cbb8d4c850c7cf6bb47d798a
Raul H <raul.dev@hotmail.com>
2025-05-28 23:11:50 -0300

    adicionando classe de exemplo: Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+public class Cliente implements Runnable{
 
-public class Cliente {
+    private ContaBancaria conta;
+
+    public Cliente(ContaBancaria conta) {
+        this.conta = conta;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for(int i = 0; i<10;i++) {
+                conta.depositar(100);
+                Thread.sleep(100);
+                conta.sacar(100);
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
