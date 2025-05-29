ff371f9334850e884dc5341ae3208fea77d2ef64
Raul Holanda <123959950+sertaoboy@users.noreply.github.com>
2025-05-29 12:44:49 -0300

    Update Aplicacao.java

--- src/localizador/Aplicacao.java
+++ src/localizador/Aplicacao.java
@@ -1,9 +1,5 @@
 package localizador;
 
-import leitura.Escritor;
-import leitura.Info;
-import leitura.Leitor;
-
 import java.io.File;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -11,10 +7,7 @@ import java.util.concurrent.FutureTask;
 
 public class Aplicacao {
 
-    private static int NUM_CARROS = 100;
-
     public static void main(String[] args) throws Exception {
-
         ExecutorService executorSimples2 = Executors.newSingleThreadExecutor();
         Localizador l = new Localizador("/home/raul/Códigos/ThreadsJava/ConcurrentsThreads1/src/localizador/teste", "arquivoteste.txt");
         FutureTask<File> task = new FutureTask<File>(l);
