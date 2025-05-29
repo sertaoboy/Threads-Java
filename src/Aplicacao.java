679bb2ac84bcc619eb2d5a639ba4a5ae161d1a02
Raul H <raul.dev@hotmail.com>
2025-05-28 23:11:16 -0300

    Instanciando ExecutorService, interface Runnable, classes de Conta e Cliente

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Aplicacao {
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        ExecutorService executorPool = Executors.newFixedThreadPool(2);
+        MyThread t = new MyThread();
+        executor.execute(t);
+        executor.shutdown();
+
+        ContaBancaria conta = new ContaBancaria();
+        Cliente c1 = new Cliente(conta);
+        Cliente c2 = new Cliente(conta);
+        executorPool.execute(c1);
+        executorPool.execute(c2);
+        executorPool.shutdown();
+        while(!executorPool.isTerminated()) {
+            Thread.sleep(1000);
+        }
+
+
+    }
 }
