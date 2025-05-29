a2bb58871b64d25b9733b8fa64cc9dd971dc3156
Raul H <raul.dev@hotmail.com>
2025-05-29 01:23:48 -0300

    implementação do método de busca por arquivo em um sistema de arquivos

--- src/Localizador.java
+++ src/Localizador.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.io.File;
 
 public class Localizador {
+
+    private String startDir;
+    private String nomeArquivo;
+
+    public Localizador(String startDir, String nomeArquivo) {
+        this.nomeArquivo=nomeArquivo;
+        this.startDir=startDir;
+    }
+
+    private File search(File dir) {
+        File[] files = dir.listFiles();
+        File result = null;
+        if(files == null) {
+            return null;
+        }
+        for(File file : files) {
+            if(file.isDirectory()) {
+                result = search(file); //recursão
+                if(result!=null) {
+                    break;
+                }
+            } else {
+                if(file.getName().equalsIgnoreCase(nomeArquivo)) {
+                    return file;
+                }
+            }
+        }
+        return result;
+    }
 }
