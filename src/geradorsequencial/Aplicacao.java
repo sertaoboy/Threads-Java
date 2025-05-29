fdfb3480395e053b7dc2e10bcf03d73a6f129840
Raul Holanda <123959950+sertaoboy@users.noreply.github.com>
2025-05-29 12:43:49 -0300

    Update Aplicacao.java
    removendo imports

--- src/geradorsequencial/Aplicacao.java
+++ src/geradorsequencial/Aplicacao.java
@@ -1,21 +1,11 @@
 package geradorsequencial;
 
-import leitura.Escritor;
-import leitura.Info;
-import leitura.Leitor;
-import localizador.Localizador;
-import testandolock.Carro;
-import testandolock.Semaforo;
-
 import java.io.File;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 
 public class Aplicacao {
-
-    private static int NUM_CARROS = 100;
-
     public static void main(String[] args) throws Exception {
         for(int i = 0; i< 10; i++) {
             int seq = Gerador.getNextSeq();
