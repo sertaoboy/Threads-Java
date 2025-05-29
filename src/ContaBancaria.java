66cd20e408f57bab8648dd8d5346cce7e9ebe734
Raul H <raul.dev@hotmail.com>
2025-05-28 23:12:36 -0300

    adicionando classe de exemplo: ContaBancaria

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,2 +1,31 @@
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ContaBancaria {
+
+    private double saldo;
+    private ReentrantLock lock = new ReentrantLock();
+
+    public void depositar(double valor) {
+        lock.lock();
+        try {
+            saldo = saldo + valor;
+            System.out.println(getSaldo());
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void sacar(double valor) {
+        lock.lock();
+        try {
+            saldo = saldo - valor;
+            System.out.println(getSaldo());
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 }
