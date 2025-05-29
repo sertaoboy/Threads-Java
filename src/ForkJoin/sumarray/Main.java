6ebd0c6eef1ac7e81e89a0f7b003e39f7665e020
Raul H <raul.dev@hotmail.com>
2025-05-29 12:39:35 -0300

    testando a soma de elementos de um vetor através do multithreading com subdivisão de tasks

--- src/ForkJoin/sumarray/Main.java
+++ src/ForkJoin/sumarray/Main.java
@@ -1,4 +1,17 @@
 package ForkJoin.sumarray;
 
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+
 public class Main {
+
+    public static void main(String[] args) {
+        int [] arr = new int[100000000];
+        Arrays.fill(arr,1);
+        SumArrayTask task = new SumArrayTask(arr,0,arr.length-1);
+        ForkJoinPool pool = new ForkJoinPool(); //criação do pool de threads para executar as tasks nos processadores disponíveis
+        int soma = pool.invoke(task);
+        System.out.println(soma);
+
+    }
 }
